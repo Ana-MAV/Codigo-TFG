73c8e5e983f4736febcebef2b9ad5097473f9700
Ana-MAV <67283883+Ana-MAV@users.noreply.github.com>
2020-07-02 10:32:51 +0200

    Update analisis_supervivencia_KM.r

--- analisis_supervivencia_KM.r
+++ analisis_supervivencia_KM.r
@@ -14,7 +14,7 @@ km_fit <- survfit(Surv(dias_min, Censored) ~ 1, data=datos)
 km_fit_max <- survfit(Surv(dias_max, Censored) ~ 1, data=datos)
 km_fit_med <- survfit(Surv(dias_media, Censored) ~ 1, data=datos)
 
-#Hacemos las gráficas de las curvas
+#Hacemos las grÃ¡ficas de las curvas
 fit <- list(SetFechaMinima = km_fit, SetFechaMaxima = km_fit_max, SetFechaMedia = km_fit_med)
 ggsurvplot_combine(fit,
                    data=datos,
@@ -70,96 +70,3 @@ surv_diff_min <- survdiff(Surv(dias_min, Censored) ~ Revista, data = datos_filtr
 surv_diff_min #Es un test log-rank
 surv_diff_max <- survdiff(Surv(dias_max, Censored) ~ Revista, data = datos_filtrados)
 surv_diff_max #Es un test log-rank
-
-
-
-#Citaciones
-#Separando por la mediana
-surv_mediana_min<-survfit(Surv(dias_min, Censored) ~ GrupoMediana, data=datos_filtrados)
-ggsurvplot(surv_mediana_min,
-                palette = c("skyblue4","chocolate3"),
-                conf.int = TRUE,
-                censor =TRUE,
-                legend = "right",
-                legend.title = "Set de datos",
-                ggtheme = theme_gray(),
-                title = "A)",
-                legend.labs=c("Datos menor o igual a 13","Datos mayores a 13"))+
-  labs(x="Dias",y="S(t)")
-
-surv_mediana <- survfit(Surv(dias_media, Censored) ~ GrupoMediana, data=datos_filtrados)
-ggsurvplot(surv_mediana,
-                palette = c("skyblue4","chocolate3"),
-                conf.int = TRUE,
-                censor =TRUE,
-                legend = "right",
-                legend.title = "Set de datos",
-                ggtheme = theme_gray(),
-                title = "B)",
-                legend.labs=c("Datos menor o igual a 13","Datos mayores a 13"))+
-  labs(x="Dias",y="S(t)")
-
-surv_mediana_max <- survfit(Surv(dias_max, Censored) ~ GrupoMediana, data=datos_filtrados)
-ggsurvplot(surv_mediana_max,
-                palette = c("skyblue4","chocolate3"),
-                conf.int = TRUE,
-                censor =TRUE,
-                legend = "right",
-                legend.title = "Set de datos",
-                ggtheme = theme_gray(),
-                title = "C)",
-                legend.labs=c("Datos menor o igual a 13","Datos mayores a 13"))+
-  labs(x="Dias",y="S(t)")
-
-#Los test log-rank para saber si son diferentes las curvas de las medianas
-surv_diff_mediana_min <- survdiff(Surv(dias_min, Censored) ~ GrupoMediana, data = datos_filtrados)
-surv_diff_mediana_min
-
-surv_diff_mediana <- survdiff(Surv(dias_media, Censored) ~ GrupoMediana, data = datos_filtrados)
-surv_diff_mediana
-
-surv_diff_mediana_max <- survdiff(Surv(dias_max, Censored) ~ GrupoMediana, data = datos_filtrados)
-surv_diff_mediana_max
-
-#Separando las citaciones por cuartiles
-surv_cuantiles_min <- survfit(Surv(dias_min, Censored) ~ GrupoCuantiles, data=datos_filtrados)
-a1 <- ggsurvplot(surv_cuantiles_min,
-                 palette = "Dark2",
-                 conf.int = TRUE,
-                 censor =TRUE,
-                 legend = "right",
-                 legend.title = "Set de datos",
-                 ggtheme = theme_gray(),
-                 title = "A)",
-                 legend.labs=c("Datos menor o igual a 4","Datos mayores a 4 y menores o iguales a 13","Datos mayores que 13 y menores o iguales a 41","Datos mayores a 41"))+
-  labs(x="Dias",y="S(t)")
-surv_cuantiles <- survfit(Surv(dias_media, Censored) ~ GrupoCuantiles, data=datos_filtrados)
-b1 <- ggsurvplot(surv_cuantiles,
-                 palette = "Dark2",
-                 conf.int = TRUE,
-                 censor =TRUE,
-                 legend = "right",
-                 legend.title = "Set de datos",
-                 ggtheme = theme_gray(),
-                 title = "B)",
-                 legend.labs=c("Datos menor o igual a 4","Datos mayores a 4 y menores o iguales a 13","Datos mayores que 13 y menores o iguales a 41","Datos mayores a 41"))+
-  labs(x="Dias",y="S(t)")
-surv_cuantiles_max <- survfit(Surv(dias_max, Censored) ~ GrupoCuantiles, data=datos_filtrados)
-c1 <- ggsurvplot(surv_cuantiles_max,
-                 palette = "Dark2",
-                 conf.int = TRUE,
-                 censor =TRUE,
-                 legend = "right",
-                 legend.title = "Set de datos",
-                 ggtheme = theme_gray(),
-                 title = "C)",
-                 legend.labs=c("Datos menor o igual a 4","Datos mayores a 4 y menores o iguales a 13","Datos mayores que 13 y menores o iguales a 41","Datos mayores a 41"))+
-  labs(x="Dias",y="S(t)")
-
-#Vamos a hacer los test log-rank
-surv_diff_cuantiles_min <- survdiff(Surv(dias_min, Censored) ~ GrupoCuantiles, data = datos_filtrados)
-surv_diff_cuantiles <- survdiff(Surv(dias_media, Censored) ~ GrupoCuantiles, data = datos_filtrados)
-surv_diff_cuantiles_max <- survdiff(Surv(dias_max, Censored) ~ GrupoCuantiles, data = datos_filtrados)
-surv_diff_cuantiles_min
-surv_diff_cuantiles
-surv_diff_cuantiles_max
